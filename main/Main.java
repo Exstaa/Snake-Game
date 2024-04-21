89216cf0c820396773d07a8887d39e6007f7a36a
Exstaa <exstagithub@gmail.com>
2024-04-21 21:18:54 +0400

    Main class created.

---
+++ main/Main.java
@@ -0,0 +1,24 @@
+package az.hafizrzazade.snakegame.main;
+
+import javax.swing.JFrame;
+
+import az.hafizrzazade.snakegame.game.Game;
+
+public class Main {
+      public static void main(String[] args) throws Exception{
+    	  int boardWidth = 600;
+    	  int boardHeight = boardWidth;
+    	  
+    	  JFrame frame = new JFrame("Snake game");
+    	  frame.setVisible(true);
+    	  frame.setSize(boardWidth, boardHeight);
+    	  frame.setLocationRelativeTo(null);
+    	  frame.setResizable(false);
+    	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	  
+    	  Game snakeGame = new Game(boardWidth, boardHeight);
+    	  frame.add(snakeGame);
+    	  frame.pack();
+    	  snakeGame.requestFocus();
+      }
+}
